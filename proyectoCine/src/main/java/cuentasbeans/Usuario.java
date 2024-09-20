04d2ed594d761900532be762f0b241fa08629ea5
Nico <nicolasasperatti@gmail.com>
2024-09-20 15:37:17 -0300

    creacion de usuariobeans

---
+++ proyectoCine/src/main/java/cuentasbeans/Usuario.java
@@ -0,0 +1,55 @@
+package cuentasbeans;
+import java.io.Serializable;
+public class Usuario implements Serializable{
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String contrasenia;
+
+    public Usuario(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    
+}
