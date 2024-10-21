472ac0d89026d2c0197bc84c54684fdada041532
KittyWind <115895672+KittyWind@users.noreply.github.com>
2024-10-21 17:44:12 -0300

    creada clase UsuarioDAO base

---
+++ proyectoCine/src/main/java/proyectocine/clasesDAO/UsuarioDAO.java
@@ -0,0 +1,39 @@
+package proyectocine.clasesDAO;
+
+import java.util.List;
+
+import proyectocine.clasesbeans.Usuario;
+
+public class UsuarioDAO implements DAO<Usuario,Integer>{
+
+    @Override
+    public void add(Usuario entidad) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void update(Usuario entidad) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public List<Usuario> getAll() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Usuario getById(Integer id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+}
