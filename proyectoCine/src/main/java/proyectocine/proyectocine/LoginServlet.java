1603164aef1463a98f75edfc723a071d2cf9ad98
KittyWind <115895672+KittyWind@users.noreply.github.com>
2024-10-22 10:06:42 -0300

    modificado loginservlet

--- proyectoCine/src/main/java/proyectocine/proyectocine/LoginServlet.java
+++ proyectoCine/src/main/java/proyectocine/proyectocine/LoginServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import proyectocine.clasesDAO.DAO;
 import proyectocine.clasesDAO.UsuarioDAO;
 import proyectocine.clasesbeans.Usuario;
@@ -21,12 +22,25 @@ public class LoginServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        super.doGet(req, resp);
+        String origen = req.getParameter("origen"); // Obtengo el origen
+        req.setAttribute("deDondeViene", origen); // Lo seteo como valor para poner en el form del .jsp (ir a verlo)
+        req.getRequestDispatcher("/WEB-INF/login-page.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        super.doPost(req, resp);
+        String m = req.getParameter("mail");
+        String p = req.getParameter("password");
+        Usuario user = new UsuarioDAO().getUser(m, p);
+        if (user != null) {
+            HttpSession session = req.getSession(); // Pido la sesión actual
+            session.setMaxInactiveInterval(60*60*24); // Seteo tiempo máximo de inactividad (en segundos)
+            session.setAttribute("userLogueado", user); // Asigno la info del usuario a la sesión
+        } else {
+            req.setAttribute("hayError", true);
+            req.setAttribute("mensajeError", "Credenciales incorrectas!");
+            doGet(req, resp);
+        }
     }
 }
