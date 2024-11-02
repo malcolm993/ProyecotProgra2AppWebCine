55a968119ad5b746411589cc2b25d081319e801e
KittyWind <115895672+KittyWind@users.noreply.github.com>
2024-11-02 09:26:40 -0300

    añadiendo comentarios

--- proyectoCine/src/main/java/proyectocine/clasesDAO/FuncionDAO.java
+++ proyectoCine/src/main/java/proyectocine/clasesDAO/FuncionDAO.java
@@ -1,63 +1,75 @@
 package proyectocine.clasesDAO;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import proyectocine.clasesbeans.HorarioFuncion;
 import proyectocine.clasesbeans.Funcion;
-import proyectocine.clasesbeans.Pelicula;
-import proyectocine.clasesbeans.Sala;
+import proyectocine.clasesbeans.HorarioFuncion;
 
 public class FuncionDAO implements DAO<Funcion, Integer> {
 
-    private static int contador = 1; // Simula un id autoincremental de base de datos
-    private List<Funcion> funciones;
-    private static FuncionDAO funcionesHardcodeadas;
-    private List<String> horarios = List.of("12:00 hs", "14:00 hs", "16:00 hs", "18:00 hs", "20:00 hs");
+    // private static int contador = 1; // Simula un id autoincremental de base de datos
+    // private List<Funcion> funciones;
+    // private static FuncionDAO funcionesHardcodeadas;
+    // private List<String> horarios = List.of("12:00 hs", "14:00 hs", "16:00 hs", "18:00 hs", "20:00 hs");
 
-    private FuncionDAO(List<Sala> x, List<Pelicula> y) {
-        this.funciones = new ArrayList<>();
-        cargarFuncionesFake(x, y);
-    }
+    // private FuncionDAO(List<Sala> x, List<Pelicula> y) {
+    //     this.funciones = new ArrayList<>();
+    //     cargarFuncionesFake(x, y);
+    // }
 
-    public static FuncionDAO getInstance(List<Sala> x, List<Pelicula> y) {
-        if (funcionesHardcodeadas == null) {
-            funcionesHardcodeadas = new FuncionDAO(x, y);
-        }
+    // public static FuncionDAO getInstance(List<Sala> x, List<Pelicula> y) {
+    //     if (funcionesHardcodeadas == null) {
+    //         funcionesHardcodeadas = new FuncionDAO(x, y);
+    //     }
 
-        return funcionesHardcodeadas;
-    }
+    //     return funcionesHardcodeadas;
+    // }
+
+    // @Override
+    // public void add(Funcion funcion) {
+    //     // TODO Auto-generated method stub
+    //     UtilExceptions.checkObjetoNulo(funcion, "La funcion no pueder nula");
+    //     funcion.setId_funcion(contador);
+    //     funciones.add(funcion);
+    //     contador++;
+    // }
 
     @Override
     public void add(Funcion funcion) {
         // TODO Auto-generated method stub
         UtilExceptions.checkObjetoNulo(funcion, "La funcion no pueder nula");
-        funcion.setId_funcion(contador);
-        funciones.add(funcion);
-        contador++;
+        
     }
+
+    // @Override
+    // public void update(Funcion funcion) {
+    //     // TODO Auto-generated method stub
+    //     UtilExceptions.checkObjetoNulo(funcion, "La funcion no pueder nula");
+    //     int idx = funciones.indexOf(funcion);
+    //     if (idx > 0) {
+    //         funciones.set(idx, funcion);
+    //     }
+    // }
 
     @Override
     public void update(Funcion funcion) {
-        // TODO Auto-generated method stub
-        UtilExceptions.checkObjetoNulo(funcion, "La funcion no pueder nula");
-        int idx = funciones.indexOf(funcion);
-        if (idx > 0) {
-            funciones.set(idx, funcion);
-        }
+        
     }
+
+    // @Override
+    // public void delete(Integer id) {
+    //     this.funciones.remove(getById(id));
+    // }
 
     @Override
     public void delete(Integer id) {
-        this.funciones.remove(getById(id));
+        
     }
-
     @Override
     public List<Funcion> getAll() {
         List<Funcion> funciones = new ArrayList<>();
@@ -108,28 +120,28 @@ public class FuncionDAO implements DAO<Funcion, Integer> {
         return funcion;
     }
 
-    public void cargarFuncionesFake(List<Sala> x, List<Pelicula> y) {
-        int contSalas = 0;
-        while (x.size() > contSalas) {
-            creacionFuncionPorSala(x.get(contSalas), y);
-            contSalas++;
-        }
-    }
+    // public void cargarFuncionesFake(List<Sala> x, List<Pelicula> y) {
+    //     int contSalas = 0;
+    //     while (x.size() > contSalas) {
+    //         creacionFuncionPorSala(x.get(contSalas), y);
+    //         contSalas++;
+    //     }
+    // }
 
-    public void creacionFuncionPorSala(Sala s, List<Pelicula> y) {
-        int auxCont = 0;
-        Pelicula p;
+    // public void creacionFuncionPorSala(Sala s, List<Pelicula> y) {
+    //     int auxCont = 0;
+    //     Pelicula p;
 
-        for (String horario : horarios) {
-            p = y.get(auxCont);
-            add(new Funcion(contador, s, p, "2024-20-10", horario));
-            auxCont++;
-        }
-    }
+    //     for (String horario : horarios) {
+    //         p = y.get(auxCont);
+    //         add(new Funcion(contador, s, p, "2024-20-10", horario));
+    //         auxCont++;
+    //     }
+    // }
 
-    public List<String> getListaHorarios() {
-        return this.horarios;
-    }
+    // public List<String> getListaHorarios() {
+    //     return this.horarios;
+    // }
 
     private Funcion rsRowToFuncion(ResultSet rs) throws SQLException{
         SalaDAO salaDao = new SalaDAO();
