8359b4880c254a3e1d3460bc5817dcbddb294afc
KittyWind <115895672+KittyWind@users.noreply.github.com>
2024-11-03 11:39:17 -0300

    añadiendo comentarios

--- proyectoCine/src/main/java/proyectocine/clasesDAO/UsuarioDAO.java
+++ proyectoCine/src/main/java/proyectocine/clasesDAO/UsuarioDAO.java
@@ -2,8 +2,6 @@ package proyectocine.clasesDAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import proyectocine.clasesbeans.RolUsuario;
@@ -11,85 +9,120 @@ import proyectocine.clasesbeans.Usuario;
 
 public class UsuarioDAO implements DAO<Usuario, Integer> {
 
-    private static int contador = 1; // Simula un id autoincremental de base de datos
-    private List<Usuario> usuarios;
-    private static UsuarioDAO usuarioshardcodeados;
+    // private static int contador = 1; // Simula un id autoincremental de base de datos
+    // private List<Usuario> usuarios;
+    // private static UsuarioDAO usuarioshardcodeados;
 
-    public UsuarioDAO() {
-        this.usuarios = new ArrayList<>();
-        cargarUsuariosFake();
-    }
+    // public UsuarioDAO() {
+    //     this.usuarios = new ArrayList<>();
+    //     cargarUsuariosFake();
+    // }
 
-    public static UsuarioDAO getInstance() {
-        if (usuarioshardcodeados == null) {
-            usuarioshardcodeados = new UsuarioDAO();
-        }
-        return usuarioshardcodeados;
-    }
+    // public static UsuarioDAO getInstance() {
+    //     if (usuarioshardcodeados == null) {
+    //         usuarioshardcodeados = new UsuarioDAO();
+    //     }
+    //     return usuarioshardcodeados;
+    // }
+
+    // @Override
+    // public void add(Usuario usuario) {
+    //     // TODO Auto-generated method stub
+    //     UtilExceptions.checkObjetoNulo(usuario, "La pelicula no pueder nula");
+    //     usuario.setId(contador);
+    //     usuarios.add(usuario);
+    //     contador++;
+    // }
 
     @Override
     public void add(Usuario usuario) {
         // TODO Auto-generated method stub
         UtilExceptions.checkObjetoNulo(usuario, "La pelicula no pueder nula");
-        usuario.setId(contador);
-        usuarios.add(usuario);
-        contador++;
+        
     }
+
+    // @Override
+    // public void update(Usuario usuario) throws Exception {
+    //     // TODO Auto-generated method stub
+    //     UtilExceptions.checkObjetoNulo(usuario, "La pelicula no pueder nula");
+    //     int idx = usuarios.indexOf(usuario);
+    //     if (idx > 0) {
+    //         usuarios.set(idx, usuario);
+    //     }
+    // }
 
     @Override
     public void update(Usuario usuario) throws Exception {
         // TODO Auto-generated method stub
         UtilExceptions.checkObjetoNulo(usuario, "La pelicula no pueder nula");
-        int idx = usuarios.indexOf(usuario);
-        if (idx > 0) {
-            usuarios.set(idx, usuario);
-        }
+        
     }
+
+    // @Override
+    // public void delete(Integer id) throws Exception {
+    //     // TODO Auto-generated method stub
+    //     this.usuarios.remove(getById(id));
+    // }
 
     @Override
     public void delete(Integer id) throws Exception {
-        // TODO Auto-generated method stub
-        this.usuarios.remove(getById(id));
+        
     }
+
+    // @Override
+    // public List<Usuario> getAll() throws Exception {
+    //     // TODO Auto-generated method stub
+    //     return new ArrayList<>(this.usuarios);
+    // }
 
     @Override
     public List<Usuario> getAll() throws Exception {
         // TODO Auto-generated method stub
-        return new ArrayList<>(this.usuarios);
+        return null;
     }
+
+    // @Override
+    // public Usuario getById(Integer id) throws Exception {
+    //     // TODO Auto-generated method stub
+    //     UtilExceptions.checkNumeroNegativo(id, "El ID no puede ser negativo");
+    //     Usuario usuario = null;
+    //     Iterator<Usuario> it = this.usuarios.iterator();
+    //     while (it.hasNext() && usuario == null) {
+    //         Usuario aux = it.next();
+    //         if (aux.getId() == id) {
+    //             usuario = aux;
+    //         }
+    //     }
+    //     UtilExceptions.checkObjetoNulo(usuario, "No existe receta con id " + id);
+    //     return usuario;
+    // }
 
     @Override
     public Usuario getById(Integer id) throws Exception {
         // TODO Auto-generated method stub
         UtilExceptions.checkNumeroNegativo(id, "El ID no puede ser negativo");
         Usuario usuario = null;
-        Iterator<Usuario> it = this.usuarios.iterator();
-        while (it.hasNext() && usuario == null) {
-            Usuario aux = it.next();
-            if (aux.getId() == id) {
-                usuario = aux;
-            }
-        }
+        
         UtilExceptions.checkObjetoNulo(usuario, "No existe receta con id " + id);
         return usuario;
     }
 
     public Usuario getUser(String mail, String password) {
         Usuario usuario = null;
-        Iterator<Usuario> it = this.usuarios.iterator();
-        while (it.hasNext() && usuario == null) {
-            Usuario aux = it.next();
-            if (aux.getEmail().equals(mail) && aux.getContrasenia().equals(password)) {
-                usuario = aux;
-            }
-        }
+        // Iterator<Usuario> it = this.usuarios.iterator();
+        // while (it.hasNext() && usuario == null) {
+        //     Usuario aux = it.next();
+        //     if (aux.getEmail().equals(mail) && aux.getContrasenia().equals(password)) {
+        //         usuario = aux;
+        //     }
+        // }
         return usuario;
     }
 
-    public void cargarUsuariosFake() {
-        add(new Usuario(contador, "user1", "lastname1", "user1@gmail.com", "124", contador, RolUsuario.CLIENTE));
-        add(new Usuario(contador, "admin1", "lastname2", "admin1@gmail.com", "123", contador, RolUsuario.ADMIN));
-    }
+    // public void cargarUsuariosFake() {
+    //     add(new Usuario(contador, "user1", "lastname1", "user1@gmail.com", "124", contador, RolUsuario.CLIENTE));
+    //     add(new Usuario(contador, "admin1", "lastname2", "admin1@gmail.com", "123", contador, RolUsuario.ADMIN));
+    // }
 
     public Usuario rsRowToUsuario(ResultSet rs) throws SQLException {
         return new Usuario(
